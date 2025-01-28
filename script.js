b272ca380fff6723f9dbd6999a3cee5ee48e79c9
ismail-jr <ismail.jibriel.jr@gmail.com>
2025-01-28 03:56:25 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Marketplace ready!");
+  // You can add functionality like filtering items, handling user clicks here
+});
